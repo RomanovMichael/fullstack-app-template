dcd55fb58df6927b6208e12e563ad4204c7018be
Romanov Michael <romanov.players@gmail.com>
2025-07-28 15:10:18 +0300

    feat: add prettier

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,34 @@
 export const App = () => {
-
   const ideas = [
     {
       nick: 'idea-nick-1',
       name: 'Salam1',
-      description: 'About what'
-    }, 
+      description: 'About what',
+    },
     {
       nick: 'idea-nick-2',
       name: 'Salam2',
-      description: 'About what'
-    }, 
+      description: 'About what',
+    },
     {
       nick: 'idea-nick-3',
       name: 'Salam3',
-      description: 'About what'
-    }, 
+      description: 'About what',
+    },
     {
       nick: 'idea-nick-4',
       name: 'Salam4',
-      description: 'About what'
-    }, 
+      description: 'About what',
+    },
     {
       nick: 'idea-nick-5',
       name: 'Salam5',
-      description: 'About what'
-    }, 
+      description: 'About what',
+    },
   ]
   return (
     <div>
-      <h1> Ideas!</h1>
+      <h1>Ideas!</h1>
       {ideas.map((idea) => {
         return (
           <div key={idea.nick}>
