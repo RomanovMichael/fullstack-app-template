684d5dfa727ee494a67657780ce578543d65dfa9
Romanov Michael <romanov.players@gmail.com>
2025-07-28 13:50:26 +0300

    feat: add example of mapping elements

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,43 @@
 export const App = () => {
-  return <h1> Hello, World!</h1>;
+
+  const ideas = [
+    {
+      nick: 'idea-nick-1',
+      name: 'Salam1',
+      description: 'About what'
+    }, 
+    {
+      nick: 'idea-nick-2',
+      name: 'Salam2',
+      description: 'About what'
+    }, 
+    {
+      nick: 'idea-nick-3',
+      name: 'Salam3',
+      description: 'About what'
+    }, 
+    {
+      nick: 'idea-nick-4',
+      name: 'Salam4',
+      description: 'About what'
+    }, 
+    {
+      nick: 'idea-nick-5',
+      name: 'Salam5',
+      description: 'About what'
+    }, 
+  ]
+  return (
+    <div>
+      <h1> Ideas!</h1>
+      {ideas.map((idea) => {
+        return (
+          <div key={idea.nick}>
+            <h2>{idea.name}</h2>
+            <p>Description {idea.description}.</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
