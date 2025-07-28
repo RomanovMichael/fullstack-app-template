de4d32be32bb7787489b3496e8342ee8a0d130a0
Romanov Michael <romanov.players@gmail.com>
2025-07-28 16:51:15 +0300

    chore: test json returning

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,41 @@
 import express from 'express'
 
+const ideas = [
+  {
+    nick: 'idea-nick-1',
+    name: 'Salam1',
+    description: 'About what',
+  },
+  {
+    nick: 'idea-nick-2',
+    name: 'Salam2',
+    description: 'About what',
+  },
+  {
+    nick: 'idea-nick-3',
+    name: 'Salam3',
+    description: 'About what',
+  },
+  {
+    nick: 'idea-nick-4',
+    name: 'Salam4',
+    description: 'About what',
+  },
+  {
+    nick: 'idea-nick-5',
+    name: 'Salam5',
+    description: 'About what',
+  },
+]
+
 const expressApp = express()
 
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
+})
+
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas)
 })
 
 expressApp.listen(3000, () => {
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,42 +1,15 @@
 export const App = () => {
-  const ideas = [
-    {
-      nick: 'idea-nick-1',
-      name: 'Salam1',
-      description: 'About what',
-    },
-    {
-      nick: 'idea-nick-2',
-      name: 'Salam2',
-      description: 'About what',
-    },
-    {
-      nick: 'idea-nick-3',
-      name: 'Salam3',
-      description: 'About what',
-    },
-    {
-      nick: 'idea-nick-4',
-      name: 'Salam4',
-      description: 'About what',
-    },
-    {
-      nick: 'idea-nick-5',
-      name: 'Salam5',
-      description: 'About what',
-    },
-  ]
   return (
     <div>
       <h1>Ideas!</h1>
-      {ideas.map((idea) => {
+      {/* {ideas.map((idea) => {
         return (
           <div key={idea.nick}>
             <h2>{idea.name}</h2>
             <p>Description {idea.description}.</p>
           </div>
         )
-      })}
+      })} */}
     </div>
   )
 }
