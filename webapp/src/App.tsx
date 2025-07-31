3cdc9aa238f8afc6e3d5e17b157d6b481956ac85
Romanov Michael <romanov.players@gmail.com>
2025-07-31 14:55:49 +0300

    test husky 2

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,7 +2,7 @@ import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 
 export const App = () => {
-  const x: number = 'ff'
+  const x: number = '1'
 
   return (
     <TrpcProvider>
