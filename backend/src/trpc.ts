5820f5df22ed3128eb8e6100522495c7ca13348b
Romanov Michael <romanov.players@gmail.com>
2025-08-01 15:39:07 +0300

    back: add trpc method

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,4 +1,5 @@
 import { initTRPC } from '@trpc/server'
+import { z } from 'zod'
 import _ from 'lodash'
 
 const ideas = _.times(100, (i) => ({
@@ -14,6 +15,16 @@ export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+      return { idea: idea || null }
+    }),
 })
 
 export type TrpcRouter = typeof trpcRouter
