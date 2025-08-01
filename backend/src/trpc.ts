1f65bdd89a13fa37dbf249a082c8b51e3d475af2
Romanov Michael <romanov.players@gmail.com>
2025-08-01 15:23:40 +0300

    add lodash and generate dummy data

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,38 +1,18 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
 
-const ideas = [
-  {
-    nick: 'idea-nick-1',
-    name: 'Salam1',
-    description: 'About what',
-  },
-  {
-    nick: 'idea-nick-2',
-    name: 'Salam2',
-    description: 'About what',
-  },
-  {
-    nick: 'idea-nick-3',
-    name: 'Salam3',
-    description: 'About what',
-  },
-  {
-    nick: 'idea-nick-4',
-    name: 'Salam4',
-    description: 'About what',
-  },
-  {
-    nick: 'idea-nick-5',
-    name: 'Salam5',
-    description: 'About what',
-  },
-]
+const ideas = _.times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}...`,
+  text: _.times(100, (j) => `<p> Paragraph ${j} of idea ${j}...</p>`).join(''),
+}))
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
-    return { ideas }
+    return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
 })
 
