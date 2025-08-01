dc0993a12c2857332f196d6e7c2f880d5ff4c0a3
Romanov Michael <romanov.players@gmail.com>
2025-08-01 16:11:14 +0300

    front: add ide detail endpoint

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,17 +1,22 @@
+import { trpc } from '../../lib/trpc'
 import { useParams } from 'react-router-dom'
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as { ideaNick: string }
 
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({ ideaNick })
+
+  if (isLoading || isFetching) return <div>Загрузка...</div>
+  if (isError) return <div>Ошибка: {error.message}</div>
+  if (!data?.idea) {
+    return <span> Idea not found</span>
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>description</p>
-      <div>
-        <p>paragraph 1</p>
-        <p>paragraph 2</p>
-        <p>paragraph 3</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <p dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   )
 }
